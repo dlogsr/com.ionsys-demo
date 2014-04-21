8f365330d1b9fec40a147b0d7af996d6d094a561
Ryan Dumlao <dlogsr@gmail.com>
2014-04-21 02:38:11 -0700

    added dose capability, walking animation, beeping, other functions

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 var flashCounter = 0;
 var $flashButton = $('#flashButton');
 var $powerButton = $('#powerButton');
+var $powerButtonOff = $('#powerButtonOff');
 var $display = $('.seven-segment');
 var $walkButton = $('#walkButton');
-var $doseButton = $('#doseButton');
+var $doseUpButton = $('#doseUpButton');
 var $doseNumber = $('#doseNumber');
+var $doseButton = $('.doseButton');
+var $greenLED = $('.ledGreen');
+var $redLED = $('.ledRed');
+var $beeper = $('#beeper')[0];
+var $beeperLong = $('#beeper-long')[0];
 var powered = false;
 var on = 'on';
 var off = 'off';
+var doseButtonFirstPress = false;
+var doseLockout = false;
+var doseCount = 0;
+var greenLEDFlash, redLEDFlash;
 
 var digits = new Array;
 digits[0] = ['a', 'b', 'c', 'd', 'e', 'f'];
@@ -20,6 +30,16 @@ digits[6] = ['a','f','e','d','c','g'];
 digits[7] = ['a','b','c'];
 digits[8] = ['a','b','c','d','e','f','g'];
 digits[9] = ['a','f','g','b','c','d'];
+
+var walkpattern = [
+	['ones','a'],
+	['ones','b'],
+	['ones','c'],
+	['ones','d'],
+	['tens','d'],
+	['tens','e'],
+	['tens','f'],
+	['tens','a']];
 
 function getWindowHeight(){
 	return $(window).height();
@@ -34,26 +54,58 @@ $(document).ready(function(){
 	adjustContentSpacing('section');
 	$display.find('*').addClass('digitOff');
 
-
 	$powerButton.click('submit',function(){
 		if(!powered)
 			powerUp();
+	})
+
+	$powerButtonOff.click('submit',function(){
+		powered = false;
+		turnOffLCD();
+		turnOffAllLED();
+		doseCount = 0;
+		clearInterval(timer);
+		clearInterval(walkPatternTimer);
+		clearInterval(doseRepeatTimer);
 	})
 
 	$flashButton.click('submit',function(){
 		if(powered) flashLCD(9);
 	});
 
-	$doseButton.click('submit',function(){
-	})
+	$doseUpButton.click('submit',function(){
+		if(powered && !doseLockout) setDose();
+	});
 
 	$walkButton.click('submit',function(){
-	})
+		if(powered) walkLCD();
+	});
 
 	$doseNumber.change(function(){
 		var number = parseInt($doseNumber.val(),10);
-		if(powered) setLCDNum(number);
-	})
+		if(powered){
+			setLCDNum(number);
+			doseCount = number;
+		};
+	});
+
+	$doseButton.mousedown(function(){
+		$doseButton.addClass('doseButtonPressed');
+		if(powered && !doseLockout){
+			if (doseButtonFirstPress){
+				setDose();
+				doseButtonFirstPress = false;
+			}
+			else{
+				setTimeout(function(){ doseButtonFirstPress = true; }, 200);
+			};
+		};
+	});
+	$doseButton.mouseup(function(){
+		setTimeout(function(){
+			$doseButton.removeClass('doseButtonPressed');
+		},25);
+	});
 
 })
 
@@ -66,8 +118,18 @@ $(window).resize(function(){
 
 function powerUp(){
 	flashCounter = 0;
-	flashLCD(9);
+	$beeper.play();
+	redLEDFlash = setTimeout(function(){
+		$redLED.removeClass('hidden');
+		setTimeout(function(){
+			$redLED.addClass('hidden');
+			flashLCD(9);
+		},500);
+	},500);
 	powered = true;
+	setTimeout(function(){
+		flashGreenLED(500,3000);
+	},4000);
 }
 
 function flashLCD(limit){
@@ -75,10 +137,15 @@ function flashLCD(limit){
 	turnOffLCD();
 	var timer = setInterval(function(){
 		if(flashCounter < limit){
-			$display.find('*').toggleClass('digitOff');
+			//$display.find('*').toggleClass('digitOff');
+			segmentToggle(digits[8],'tens');
+			segmentToggle(digits[8],'ones');
 			flashCounter++;
 		}
-		else clearInterval(timer);
+		else{
+			clearInterval(timer);
+			setLCDNum(doseCount);
+		};
 	},500);
 }
 
@@ -92,20 +159,20 @@ function turnOnLCD(){
 
 function setLCDNum(number){
 	turnOffLCD();
-	console.log('setting dose counter to '+number);
+	//console.log('setting dose counter to '+number);
 	if(number>9)
 		segmentToggle(digits[Math.floor(number/10)],'tens',on);
-	segmentToggle(digits[number%10],'ones',on);
+		segmentToggle(digits[number%10],'ones',on);
 }
 
 //take in an array of segments to toggle, place (tens or ones), and toggle just those
 function segmentToggle(input,place,explicit){ 
-	console.log('toggling '+input);
+	//console.log('toggling '+input);
 	for(var segment in input)
 	{
 		segmentClass = '.'+input[segment];
 		placeClass = '.'+place;
-		console.log('segment '+input[segment]+' '+explicit+' (class '+segmentClass+')');
+		//console.log('segment '+input[segment]+' '+explicit+' (class '+segmentClass+')');
 		if(explicit == on)
 			$(placeClass).find(segmentClass).removeClass('digitOff');
 		else if (explicit == off)
@@ -117,9 +184,56 @@ function segmentToggle(input,place,explicit){
 }
 
 function walkLCD(){
-
+	var walkCounter = 0;
+	var wtPlace, wtNumber;
+	var walkPatternTimer = setInterval(function(){
+		turnOffLCD();
+		wtPlace = walkpattern[walkCounter][0];
+		wtNumber = walkpattern[walkCounter][1];
+		segmentToggle(wtNumber,wtPlace,on);
+		if(walkCounter < 7)
+			walkCounter++;
+		else
+			walkCounter = 0;
+	},200);
+	var walkTimer = setTimeout(function(){
+		clearInterval(walkPatternTimer);
+		setLCDNum(doseCount);
+	},3200);
 }
 
 function setDose(){
+	doseLockout = true;
+	turnOffLCD();
+	turnOffAllLED();
+	$beeperLong.play();
+	flashGreenLED(400,800);
+	walkLCD();
+	var doseRepeatTimer = setInterval(function(){
+		walkLCD();
+	},5600);
+	var doseTimer = setTimeout(function(){
+		clearInterval(doseRepeatTimer);
+		turnOffAllLED();
+		flashGreenLED(500,3000);
+		doseCount++;
+		setLCDNum(doseCount);
+		$doseNumber.val(doseCount);
+	},20000)
+	
+}
 
+function flashGreenLED(ontime,offtime){
+	if(powered){
+		greenLEDFlash = setInterval(function(){
+			$greenLED.removeClass('hidden');
+			setTimeout(function(){
+				$greenLED.addClass('hidden');
+			},ontime);
+		},offtime);
+	}
+}
+
+function turnOffAllLED(){
+	clearInterval(greenLEDFlash);
 }
