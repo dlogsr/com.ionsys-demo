271d98d0e6ed9d768486bfc6d331e0e1649df8af
Ryan Dumlao <dlogsr@gmail.com>
2015-11-05 13:51:50 -0800

    debug sound issue 27

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ document.addEventListener("touchend", function ()
 	had_touch = true;
 });
 
-document.addEventListener("load", function ()
+document.addEventListener("tap", function ()
 {
 	if (had_touch)
 		return;
